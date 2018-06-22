07a85130d87b632799fc331595d0d6650645fec3
Christofer Chan <devchumbing@gmail.com>
2018-06-22 08:47:38 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@
    new_languages = {}
    languages.each do |type, hash|
      hash.each do |name, attributes|
-       new_languages[name] = attributes
+       new_languages[name] ||= attributes
        new_languages[name][:style] = []
        new_languages[name][:style] << type
      end
